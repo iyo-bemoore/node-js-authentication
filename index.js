4aecdb43758e02c54cde53a9ff832cc94e8a9281
2imad <imadyoussoufi@gmail.com>
2020-11-11 13:05:06 +0100

    add mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('./db/mongoose');
 const app = express();
 const PORT = process.env.PORT || 8000
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
